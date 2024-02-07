0f56c6a203abef41bd3365805e3692c87882aa64
Christian France <chfrance15@gmail.com>
2024-02-07 18:13:20 -0500

    initial codebase

---
+++ musclepedia/src/app/app.config.ts
@@ -0,0 +1,12 @@
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"musclepedia-daa3a","appId":"1:822438438225:web:8f9e49ded3e53064aa9f70","storageBucket":"musclepedia-daa3a.appspot.com","apiKey":"AIzaSyBQNBEkE-I112lFBRitaolLPT5BAN6B7Qs","authDomain":"musclepedia-daa3a.firebaseapp.com","messagingSenderId":"822438438225","measurementId":"G-115F4JRSG8"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideAnalytics(() => getAnalytics())), ScreenTrackingService, UserTrackingService, importProvidersFrom(provideFirestore(() => getFirestore()))]
+};
---
+++ musclepedia/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyBQNBEkE-I112lFBRitaolLPT5BAN6B7Qs",
+        authDomain: "musclepedia-daa3a.firebaseapp.com",
+        projectId: "musclepedia-daa3a",
+        storageBucket: "musclepedia-daa3a.appspot.com",
+        messagingSenderId: "822438438225",
+        appId: "1:822438438225:web:8f9e49ded3e53064aa9f70",
+        measurementId: "G-115F4JRSG8"
+    }
+}
